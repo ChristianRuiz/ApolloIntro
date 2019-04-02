5f9777cd6a8c95f3905eb2be4ec7dd7b15683c04
cruiz <christian.ruiz.mail@gmail.com>
2019-04-02 13:28:13 +0200

    List component importing apollo and ready to implement the query

--- src/components/countrieslist/list.js
+++ src/components/countrieslist/list.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import propTypes from 'prop-types';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 import { Link } from 'react-router-dom';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
@@ -7,32 +10,35 @@ import ToggleFavorite from '../common/toggleFavorite';
 
 import './list.css';
 
-const List = () => (
-  <div className="countries-list">
-    {[
-      { code: 'AR', name: 'Argentina', isFavorite: false },
-      { code: 'CA', name: 'Canada', isFavorite: true },
-      { code: 'ES', name: 'Spain', isFavorite: false },
-      { code: 'AR', name: 'Argentina', isFavorite: true },
-      { code: 'CA', name: 'Canada', isFavorite: false },
-      { code: 'ES', name: 'Spain', isFavorite: false },
-      { code: 'AR', name: 'Argentina', isFavorite: false },
-      { code: 'CA', name: 'Canada', isFavorite: true },
-      { code: 'ES', name: 'Spain', isFavorite: false }
-    ].map(country => (
-      <Link to={`/contry/${country.code}`} key={country.code}>
-        <Paper className="country-card" elevation={1}>
-          <img src={`http://www.countryflags.io/${country.code}/flat/64.png`} alt={country.code} />
-          <div className="country-card-content">
-            <Typography variant="h5" component="h3">
-              {country.name}
-            </Typography>
-          </div>
-          <ToggleFavorite isFavorite={country.isFavorite} />
-        </Paper>
-      </Link>
-    ))}
-  </div>
-);
+const List = () => {
+  return (
+    <div className="countries-list">
+      {[
+        { code: 'AR', name: 'Argentina', isFavorite: false },
+        { code: 'CA', name: 'Canada', isFavorite: true },
+        { code: 'ES', name: 'Spain', isFavorite: false }
+      ].map(country => (
+        <Link to={`/contry/${country.code}`} key={country.code}>
+          <Paper className="country-card" elevation={1}>
+            <img src={`http://www.countryflags.io/${country.code}/flat/64.png`} alt={country.code} />
+            <div className="country-card-content">
+              <Typography variant="h5" component="h3">
+                {country.name}
+              </Typography>
+            </div>
+            <ToggleFavorite code={country.code} isFavorite={country.isFavorite} />
+          </Paper>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+// List.propTypes = {
+//   data: propTypes.object.isRequired
+// };
+
+// const query = gql`
+// `;
 
 export default List;
