2037a45275d51a74223e8e8d25936ab28e242335
cruiz <christian.ruiz.mail@gmail.com>
2019-04-02 19:04:25 +0200

    Countries list refactored to use a querycomponent

--- src/components/countrieslist/list.js
+++ src/components/countrieslist/list.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import propTypes from 'prop-types';
-import { graphql } from 'react-apollo';
-import gql from 'graphql-tag';
 import { Link } from 'react-router-dom';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
@@ -9,46 +6,34 @@ import Typography from '@material-ui/core/Typography';
 import ToggleFavorite from '../common/toggleFavorite';
 import Loading from '../common/loading';
 
+import GetCountriesQuery from './querycomponents/getCountriesQuery';
+
 import './list.css';
 
-const List = ({ data }) => {
-  const { loading, countries } = data;
+const List = () => (
+  <div className="countries-list">
+    <GetCountriesQuery>
+      {({ data, loading }) =>
+        loading ? (
+          <Loading />
+        ) : (
+          data.countries.map(country => (
+            <Link to={`/contry/${country.code}`} key={country.code}>
+              <Paper className="country-card" elevation={1}>
+                <img src={`http://www.countryflags.io/${country.code}/flat/64.png`} alt={country.code} />
+                <div className="country-card-content">
+                  <Typography variant="h5" component="h3">
+                    {country.name}
+                  </Typography>
+                </div>
+                <ToggleFavorite code={country.code} isFavorite={country.isFavorite} />
+              </Paper>
+            </Link>
+          ))
+        )
+      }
+    </GetCountriesQuery>
+  </div>
+);
 
-  return (
-    <div className="countries-list">
-      {loading ? (
-        <Loading />
-      ) : (
-        countries.map(country => (
-          <Link to={`/contry/${country.code}`} key={country.code}>
-            <Paper className="country-card" elevation={1}>
-              <img src={`http://www.countryflags.io/${country.code}/flat/64.png`} alt={country.code} />
-              <div className="country-card-content">
-                <Typography variant="h5" component="h3">
-                  {country.name}
-                </Typography>
-              </div>
-              <ToggleFavorite code={country.code} isFavorite={country.isFavorite} />
-            </Paper>
-          </Link>
-        ))
-      )}
-    </div>
-  );
-};
-
-List.propTypes = {
-  data: propTypes.object.isRequired
-};
-
-const query = gql`
-  query GetCountries {
-    countries {
-      name
-      code
-      isFavorite
-    }
-  }
-`;
-
-export default graphql(query)(List);
+export default List;
