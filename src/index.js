3b4c341e5eb3d42681dae09aceb9279d334cd52d
cruiz <christian.ruiz.mail@gmail.com>
2019-04-02 13:22:10 +0200

    Apollo client added to the root App

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import 'typeface-roboto';
 import './index.css';
 import App from './App';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:6060'
+});
+
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <ApolloProvider client={client}>
+    <Router>
+      <App />
+    </Router>
+  </ApolloProvider>,
   document.getElementById('root')
 );
