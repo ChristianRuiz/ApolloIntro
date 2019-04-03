1944087c77bd36ba09ee5cdea3755f1f496dbfb2
cruiz <christian.ruiz.mail@gmail.com>
2019-04-03 17:26:58 +0200

    Contry detail component refactored to use hooks

--- src/components/countrydetail/detail.js
+++ src/components/countrydetail/detail.js
@@ -6,53 +6,49 @@ import Typography from '@material-ui/core/Typography';
 import ToggleFavorite from '../common/toggleFavorite';
 import Loading from '../common/loading';
 
-import GetCountryByCodeQuery from './querycomponents/getCountryByCodeQuery';
+import useGetCountryByCodeQuery from './querycomponents/useGetCountryByCodeQuery';
 
 import './detail.css';
 
 const Detail = ({ match }) => {
   const code = match.params.code;
+  const { loading, data } = useGetCountryByCodeQuery(code);
+  const { country } = data;
 
   return (
     <div className="country-detail">
       <Paper elevation={1} key={code} className="country-detail-card">
-        <GetCountryByCodeQuery code={code}>
-          {({ loading, data }) => {
-            const { country } = data;
-
-            return loading ? (
-              <Loading />
-            ) : (
-              <Fragment>
-                <div className="country-detail-card-content">
-                  <img src={`https://www.countryflags.io/${code}/flat/64.png`} alt={code} />
-                  <Typography variant="h5" component="h3">
-                    {country.name}
+        {loading ? (
+          <Loading />
+        ) : (
+          <Fragment>
+            <div className="country-detail-card-content">
+              <img src={`https://www.countryflags.io/${code}/flat/64.png`} alt={code} />
+              <Typography variant="h5" component="h3">
+                {country.name}
+              </Typography>
+              <Typography component="p">
+                <b>Continent: </b>
+                {country.continent.name}
+              </Typography>
+              <Typography component="p">
+                <b>Currency: </b>
+                {country.currency}
+              </Typography>
+              <Typography component="p">
+                <b>Languages</b>
+              </Typography>
+              <ul>
+                {country.languages.map(language => (
+                  <Typography component="li" key={language.name}>
+                    {language.name}
                   </Typography>
-                  <Typography component="p">
-                    <b>Continent: </b>
-                    {country.continent.name}
-                  </Typography>
-                  <Typography component="p">
-                    <b>Currency: </b>
-                    {country.currency}
-                  </Typography>
-                  <Typography component="p">
-                    <b>Languages</b>
-                  </Typography>
-                  <ul>
-                    {country.languages.map(language => (
-                      <Typography component="li" key={language.name}>
-                        {language.name}
-                      </Typography>
-                    ))}
-                  </ul>
-                </div>
-                <ToggleFavorite code={code} isFavorite={country.isFavorite} />
-              </Fragment>
-            );
-          }}
-        </GetCountryByCodeQuery>
+                ))}
+              </ul>
+            </div>
+            <ToggleFavorite code={code} isFavorite={country.isFavorite} />
+          </Fragment>
+        )}
       </Paper>
     </div>
   );
