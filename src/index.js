ef25954e17e47bb0557629745ffb6826bd7b02ef
cruiz <christian.ruiz.mail@gmail.com>
2019-04-03 17:25:21 +0200

    Added react-apollo-hooks package and Provider configured

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks';
 import 'typeface-roboto';
 import './index.css';
 import App from './App';
@@ -13,9 +14,11 @@ const client = new ApolloClient({
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Router>
-      <App />
-    </Router>
+    <ApolloHooksProvider client={client}>
+      <Router>
+        <App />
+      </Router>
+    </ApolloHooksProvider>
   </ApolloProvider>,
   document.getElementById('root')
 );
