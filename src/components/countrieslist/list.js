2f0e9d6db9b334e8bcd38e22261fbe4fd58572b4
cruiz <christian.ruiz.mail@gmail.com>
2019-04-03 13:24:16 +0200

    Displaying both loading animation and cached data while refetching

--- src/components/countrieslist/list.js
+++ src/components/countrieslist/list.js
@@ -5,7 +5,6 @@ import Typography from '@material-ui/core/Typography';
 import Autorenew from '@material-ui/icons/Autorenew';
 
 import ToggleFavorite from '../common/toggleFavorite';
-import Loading from '../common/loading';
 
 import GetCountriesQuery from './querycomponents/getCountriesQuery';
 
@@ -14,30 +13,35 @@ import './list.css';
 const List = () => (
   <div className="countries-list">
     <GetCountriesQuery>
-      {({ data, loading, refetch }) =>
-        loading ? (
-          <Loading />
-        ) : (
-          <Fragment>
-            <div className="countries-list-refresh">
-              <Autorenew color="secondary" fontSize="large" onClick={() => refetch()} />
-            </div>
-            {data.countries.map(country => (
-              <Link to={`/contry/${country.code}`} key={country.code}>
-                <Paper className="country-card" elevation={1}>
-                  <img src={`http://www.countryflags.io/${country.code}/flat/64.png`} alt={country.code} />
-                  <div className="country-card-content">
-                    <Typography variant="h5" component="h3">
-                      {country.name}
-                    </Typography>
-                  </div>
-                  <ToggleFavorite code={country.code} isFavorite={country.isFavorite} />
-                </Paper>
-              </Link>
-            ))}
-          </Fragment>
-        )
-      }
+      {({ data, loading, refetch }) => (
+        <Fragment>
+          <div className="countries-list-refresh">
+            <Autorenew
+              color="secondary"
+              fontSize="large"
+              onClick={() => refetch()}
+              className={loading ? 'rotate' : ''}
+            />
+          </div>
+          {data && data.countries ? (
+            <Fragment>
+              {data.countries.map(country => (
+                <Link to={`/contry/${country.code}`} key={country.code}>
+                  <Paper className="country-card" elevation={1}>
+                    <img src={`http://www.countryflags.io/${country.code}/flat/64.png`} alt={country.code} />
+                    <div className="country-card-content">
+                      <Typography variant="h5" component="h3">
+                        {country.name}
+                      </Typography>
+                    </div>
+                    <ToggleFavorite code={country.code} isFavorite={country.isFavorite} />
+                  </Paper>
+                </Link>
+              ))}
+            </Fragment>
+          ) : null}
+        </Fragment>
+      )}
     </GetCountriesQuery>
   </div>
 );
