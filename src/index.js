b504963ecfdf19433f6a6cbc2228d99dd14090f2
cruiz <christian.ruiz.mail@gmail.com>
2019-04-04 10:57:58 +0200

    MIgrated from apollo-boost to adding all the apollo packages manually (https://www.apollographql.com/docs/react/advanced/boost-migration)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import ApolloClient from 'apollo-boost';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
 import { ApolloProvider } from 'react-apollo';
 import 'typeface-roboto';
 import './index.css';
 import App from './App';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:6060'
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
+        );
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    }),
+    new HttpLink({
+      uri: 'http://localhost:6060'
+    })
+  ]),
+  cache: new InMemoryCache()
 });
 
 ReactDOM.render(
